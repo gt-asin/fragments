402f914591e9944cdbf61366401a39e5b30accd6
Anthony <149127115+gt-asin@users.noreply.github.com>
2025-03-31 08:38:07 -0400

    Lab 9 s3 Implementation

---
+++ src/routes/api/delete.js
@@ -0,0 +1,26 @@
+// src/routes/api/delete.js
+
+const logger = require('../../logger');
+const { Fragment } = require('../../model/fragment');
+const { createSuccessResponse, createErrorResponse } = require('../../response');
+
+module.exports = async (req, res) => {
+  const fragmentId = req.params.id;
+  const ownerId = req.user;
+
+  logger.info('DELETE /fragments/:id');
+
+  try {
+    await Fragment.delete(ownerId, fragmentId);
+
+    logger.info(`Fragment was deleted`);
+
+    const successResponse = createSuccessResponse({});
+    return res.status(200).json(successResponse);
+  } catch (err) {
+    const errorResponse = createErrorResponse(404, err.message);
+    logger.error({ errorResponse }, 'Failed to delete the fragment');
+
+    return res.status(404).json(errorResponse);
+  }
+};
